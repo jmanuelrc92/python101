b5f6b42d4729a7f7f7d04e5e2835289e4b44a7eb
jmanuelRC <juan.02324@gmail.com>
2020-02-16 17:30:36 -0600

    Execise 8 added

---
+++ exercise8/exerciseEight.py
@@ -0,0 +1,18 @@
+"""Ejercicio 8
+Un vendedor recibe un sueldo base mas un 10% extra por comisión de sus ventas,
+el vendedor desea saber cuanto dinero obtendrá por concepto de comisiones
+por las tres ventas que realiza en el mes y el total que recibirá en el mes
+tomando en cuenta su sueldo base y comisiones."""
+comisiones = 0.0
+sueldoBase = float(input("Ingrese sueldo base: "))
+ventasDelMes = [0,0,0]
+i = 0
+while i < 3:
+    ventasDelMes[i] = float(input("Ingrese una venta: "))
+    i = i + 1
+i = 0
+while i <3:
+    comisiones += ventasDelMes[i]*0.01
+    i = i + 1
+print("Total por comisiones: " + str(comisiones))
+print("Total del mes: " + str(comisiones + sueldoBase))
